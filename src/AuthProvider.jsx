685bf25aa650e8681d6d54edc25a0c09eae9a1a7
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-10 10:08:33 +0200

    add current user to authProvider

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,17 +1,26 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [logged, setLogged] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
+  const baseURL = process.env.REACT_APP_API_URL;
+  const base = axios.create({ baseURL });
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const hasJwt = localStorage.getItem("jwt");
     setLogged(Boolean(hasJwt));
-  }, []);
+    if (hasJwt) {
+      getCurrentUser();
+    }
+  }, [logged]);
+
+  console.log(currentUser);
 
   function disconnect() {
     navigate("/");
@@ -19,7 +28,16 @@ export default function AuthProvider({ children }) {
     localStorage.removeItem("jwt");
   }
 
-  const value = { logged, setLogged, disconnect };
+  const getCurrentUser = () => {
+    const token = localStorage.getItem("jwt");
+    return base
+      .get(`/admins`, { headers: { Authorization: `Bearer ${token}` } })
+      .then((res) => {
+        setCurrentUser(res.data);
+      });
+  };
+
+  const value = { logged, setLogged, disconnect, currentUser, setCurrentUser };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,39 +4,6 @@ import styled, { css } from "styled-components";
 import About from "../../components/Home_Components/About/About";
 
 export default function Home() {
-  const Container = styled.div`
-    height: 100vh;
-    overflow: hidden;
-    position: relative;
-  `;
-
-  const Shape = css`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-  `;
-
-  const HeaderShape = styled.div`
-    ${Shape}
-    clip-path: polygon(0% 0%, 55% 0%, 65% 100%, 0% 100%);
-    background-color: var(--color-primary);
-    @media only screen and (max-width: 1024px) {
-      display: none;
-    }
-  `;
-
-  const AboutShape = styled.div`
-    ${Shape}
-    clip-path: polygon(65% 0%, 100% 0%, 100% 100%, 65% 100%);
-    background-color: var(--color-primary);
-    @media only screen and (max-width: 1024px) {
-      display: none;
-    }
-  `;
-
   return (
     <section>
       <Container>
@@ -50,3 +17,36 @@ export default function Home() {
     </section>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+`;
+
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const HeaderShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0% 0%, 55% 0%, 65% 100%, 0% 100%);
+  background-color: var(--color-primary);
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const AboutShape = styled.div`
+  ${Shape}
+  clip-path: polygon(65% 0%, 100% 0%, 100% 100%, 65% 100%);
+  background-color: var(--color-primary);
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
