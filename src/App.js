e2ceb6a608335660ee2ff0d2f896bdb9f257d23d
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-14 09:13:12 +0200

    new navbar ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import { useContext, useState } from "react";
+import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import { AuthContext } from "./AuthProvider";
-import Navbar from "./components/Navbar/Navbar";
-
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import Forgot from "./pages/Login/Forgot";
 import Login from "./pages/Login/Login";
 import Reset from "./pages/Login/Reset";
 import Profile from "./pages/Profile/Profile";
-import EventDetails from "./pages/EventDetails/EventDetails";
 import Signin from "./pages/Signin/Signin";
 import Register from "./pages/Register/Register";
 import EventDetailsAndTab from "./pages/EventDetails/EventDetailsAndTab";
+import NavbarNew from "./components/Navbar/NavbarNew";
 
 function App() {
-  const { logged, setLogged } = useContext(AuthContext);
-
   return (
     <>
-      <Navbar />
+      <NavbarNew />
       <Routes>
         <Route element={<Home />} path="/" exact />
         <Route element={<Dashboard />} path="/dashboard" exact />
