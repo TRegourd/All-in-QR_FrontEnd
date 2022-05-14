85d9fab83e7d921ca28c92346db8d753ad0ad5f6
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-14 09:14:23 +0200

    old navbar removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
