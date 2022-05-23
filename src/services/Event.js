e34b670997edb09d649d2782a6c38b293fef090b
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-23 11:51:59 +0200

    protected routes

--- src/services/Event.js
+++ src/services/Event.js
@@ -19,7 +19,12 @@ const eventServices = {
   },
 
   getOneEvent(id) {
-    return base.get("/events/" + id).then((res) => res.data);
+    const token = localStorage.getItem("jwt");
+    return base
+      .get("/events/" + id, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => res.data);
   },
 
   modifyOneEvent(id, body) {
